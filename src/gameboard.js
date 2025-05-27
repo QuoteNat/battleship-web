95b33e8ed8dd39859b3d28c115080d7bda927c3b
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-27 15:36:27 -0500

    Receive attack

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,39 +1,83 @@
-import Ship from "./ship.js"
+import Ship from "./ship.js";
 import Vector from "./vector.js";
 export const DIRECTIONS = Object.freeze({
-    UP: 0,
-    RIGHT: 1,
-    DOWN: 2,
-    LEFT: 3,
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 2,
+  LEFT: 3,
 });
 
-export class Gameboard {
-    ships = [];
-    hits = [];
-    /**
-     * Places a new ship on the gameboard
-     * @param {*} length length of the ship
-     * @param {[integer, integer]} coordinate first coordinate of the ship
-     * @param {[integer, integer]} direction the direction of the ship, going from coordinate
-     * @returns A reference to the placed ship
-     */
-    placeShip(length, coordinate, direction) {
-        let newShip = {
-            ship: new Ship(length),
-            coordinate: Vector.from(coordinate),
-            direction: direction
-        };
-        this.ships.push(newShip);
-        return newShip;
-    }
+function shipHitDetect(shipContainer, coordinate) {
+  switch (shipContainer.direction) {
+    case DIRECTIONS.UP:
+      if (
+        coordinate[1] >= shipContainer.coordinate[1] &&
+        coordinate[1] <= shipContainer.coordinate[1] + shipContainer.length &&
+        coordinate[0] == shipContainer.coordinate[0]
+      )
+        return true;
+      break;
+    case DIRECTIONS.DOWN:
+      if (
+        coordinate[1] <= shipContainer.coordinate[1] &&
+        coordinate[1] >=
+          shipContainer.coordinate[1] - shipContainer.ship.length &&
+        coordinate[0] == shipContainer.coordinate[0]
+      )
+        return true;
+      break;
+    case DIRECTIONS.LEFT:
+      if (
+        coordinate[0] <= shipContainer.coordinate[0] &&
+        coordinate[0] >=
+          shipContainer.coordinate[0] - shipContainer.ship.length &&
+        coordinate[1] == shipContainer.coordinate[1]
+      )
+        return true;
+      break;
+    case DIRECTIONS.RIGHT:
+      if (
+        coordinate[0] >= shipContainer.coordinate[0] &&
+        coordinate[0] <=
+          shipContainer.coordinate[0] + shipContainer.ship.length &&
+        coordinate[1] == shipContainer.coordinate[1]
+      )
+        return true;
+      break;
+  }
+  return false;
+}
 
-    receiveAttack(coordinate) {
-        for (const shipContainer of this.ships) {
-            if (shipContainer.coordinate.isEqualTo(coordinate)) {
-                shipContainer.ship.hit();
-                return true;
-            };
-        }
-        return false;
+export class Gameboard {
+  ships = [];
+  hits = [];
+  /**
+   * Places a new ship on the gameboard
+   * @param {*} length length of the ship
+   * @param {[integer, integer]} coordinate first coordinate of the ship
+   * @param {[integer, integer]} direction the direction of the ship, going from coordinate
+   * @returns A reference to the placed ship
+   */
+  placeShip(length, coordinate, direction) {
+    let newShip = {
+      ship: new Ship(length),
+      coordinate: Vector.from(coordinate),
+      direction: direction,
+    };
+    this.ships.push(newShip);
+    return newShip;
+  }
+
+  receiveAttack(coordinate) {
+    for (const shipContainer of this.ships) {
+      if (
+        shipContainer.coordinate.isEqualTo(coordinate) ||
+        shipHitDetect(shipContainer, coordinate)
+      ) {
+        shipContainer.ship.hit();
+        return true;
+      }
     }
+    return false;
+  }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,15 +1,20 @@
-import {Gameboard, DIRECTIONS} from "./gameboard.js";
+import { Gameboard, DIRECTIONS } from "./gameboard.js";
 let gameboard;
-beforeEach(() => { gameboard = new Gameboard(); });
-
-test("Place ship", () => {
-    let newShip = gameboard.placeShip(2, [1, 1], DIRECTIONS.DOWN);
-    expect(newShip).not.toBeNull();
+beforeEach(() => {
+  gameboard = new Gameboard();
 });
 
-describe('Receive attack tests', () => {
-    beforeEach(() => gameboard.placeShip(5, [0, 0], DIRECTIONS.RIGHT));
-    test("Receive attack at start", () => expect(gameboard.receiveAttack([0, 0])).toBe(true));
-    test('Receive attack in middle', () => expect(gameboard.receiveAttack([0, 1])).toBe(true));
-    test('Receive attack after end', () => expect(gameboard.receiveAttack([0, 5])).toBe(false));
-})
+test("Place ship", () => {
+  let newShip = gameboard.placeShip(2, [1, 1], DIRECTIONS.DOWN);
+  expect(newShip).not.toBeNull();
+});
+
+describe("Receive attack", () => {
+  beforeEach(() => gameboard.placeShip(5, [0, 0], DIRECTIONS.RIGHT));
+  test("Receive attack at start", () =>
+    expect(gameboard.receiveAttack([0, 0])).toBe(true));
+  test("Receive attack in middle", () =>
+    expect(gameboard.receiveAttack([1, 0])).toBe(true));
+  test("Receive attack after end", () =>
+    expect(gameboard.receiveAttack([0, 5])).toBe(false));
+});
