be08cc195ef02408f8eaf948fab7977d0635ffde
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-06-10 14:11:08 -0500

    feat: Add Player and Cpu classes

--- src/player.js
+++ src/player.js
@@ -1,7 +1,21 @@
 import { Gameboard } from "./gameboard";
 
-export default class Player {
+export class Player {
+  previousMoves = [];
   constructor() {
     this.gameboard = new Gameboard();
+  }
+}
+
+function randInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export class Cpu extends Player {
+  doMove() {
+    let x = randInt(10);
+    let y = randInt(10);
+    this.gameboard.receiveAttack([x, y]);
+    return [x, y];
   }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -1,5 +1,11 @@
-import Player from "./player";
-
+import { Player, Cpu } from "./player";
 test("Player constructor", () => {
   expect(new Player()).not.toBeNull();
+});
+
+test("Random move", () => {
+  let cpu = new Cpu();
+  let move = cpu.doMove();
+  expect(move[0]).not.toBeNull();
+  expect(move[1]).not.toBeNull();
 });
