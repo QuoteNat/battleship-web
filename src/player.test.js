5d3114f6a7df9ba4db9558e7404bd4e928306f5c
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-06-25 11:29:34 -0500

    Fix player.test.js

--- src/player.test.js
+++ src/player.test.js
@@ -1,8 +1,10 @@
 import expect from "expect";
 import { DIRECTIONS } from "./gameboard";
 import { Player, Cpu } from "./player";
+const shipLengths = [];
+
 test("Player constructor", () => {
-  expect(new Player()).not.toBeNull();
+  expect(new Player(shipLengths)).not.toBeNull();
 });
 
 test("CPU move", () => {
@@ -13,29 +15,29 @@ test("CPU move", () => {
 });
 
 test("Player move", () => {
-  let player = new Player();
+  let player = new Player(shipLengths);
   expect(player.doMove([1, 1])).toBe(true);
   expect(player.doMove([1, 1])).toBe(false);
 });
 
 test("Place ship", () => {
-  let player = new Player();
+  let player = new Player(shipLengths);
   expect(player.placeShip([1, 1], DIRECTIONS.DOWN, 2)).toBe(true);
 });
 
 test("Can't place ship out of bounds", () => {
-  let player = new Player();
+  let player = new Player(shipLengths);
   expect(player.placeShip([-1, -1], DIRECTIONS.DOWN, 2)).toBe(false);
 });
 
 test("Can't place a ship inside another ship", () => {
-  let player = new Player();
+  let player = new Player(shipLengths);
   player.placeShip([1, 1], DIRECTIONS.DOWN, 2);
   expect(player.placeShip([1, 1], DIRECTIONS.DOWN, 2)).toBe(false);
 });
 
 test("Player can tell whether it has lost or not", () => {
-  let player = new Player();
+  let player = new Player(shipLengths);
   player.placeShip([1, 1], DIRECTIONS.DOWN, 1);
   expect(player.hasLost()).toBe(false);
   player.receiveAttack([1, 1]);
